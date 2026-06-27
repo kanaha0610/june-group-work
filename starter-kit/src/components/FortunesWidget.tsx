b7b959457a8ec6d1aa2cfd2d8466f2a0674447be
sumireGH <awagesah.erimus@gmail.com>
2026-06-27 18:19:57 +0900

    FortunesWidget.tsxの中身をデフォルトから変更しなおしました

--- starter-kit/src/components/FortunesWidget.tsx
+++ starter-kit/src/components/FortunesWidget.tsx
@@ -1,4 +1,17 @@
-import { CheckCircle } from 'lucide-react';
+import React from 'react';
+
+export default function UranaiWidget(){
+    return(
+        <section className="bg-white p-6 rounded-2xl shadow-sm border border-slate-100 hover:shadow-md transition-shadow">
+            <h2 className="font-bold text-state-800 text-center">
+                【今日の運勢】
+            </h2>
+            <div></div>
+        </section>
+    )
+}
+
+/*import { CheckCircle } from 'lucide-react';
 
 function FortunesWidget() {
   return (
@@ -18,4 +31,4 @@ function FortunesWidget() {
   );
 }
 
-export default FortunesWidget;
+export default FortunesWidget;*/
